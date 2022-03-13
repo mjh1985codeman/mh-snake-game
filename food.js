245f0075ab920487e2d154d165b8e3e7068278d3
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2022-03-12 21:27:26 -0600

    styled the food to look like an apple

--- food.js
+++ food.js
@@ -15,7 +15,7 @@ export function update() {
 }
 
 export function draw(gameBoard) {
-  const foodElement = document.createElement("div");
+  const foodElement = document.createElement("image");
   foodElement.style.gridRowStart = food.y;
   foodElement.style.gridColumnStart = food.x;
   foodElement.classList.add("food");
